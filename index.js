af2f0da3054658b40eb794d7536e70817c09fa5d
marcodagolini <marco.dagolini@gmail.com>
2021-03-08 23:43:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,7 @@ function callApi(){
   var URLforPOST = "https://marcowabot.herokuapp.com/outboundWhatsapp";
   xhr.open("POST", URLforPOST);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "https://marcodagolini.github.io");
-  xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
+
   var myBodyToSend = JSON.stringify({"internationalCode": internationalCode, "number": number, "siteId": siteId, "skill": skill, "proactiveTemplate": proactiveTemplate, "proactivevariables": proactivevariables});
   xhr.send(myBodyToSend);
   
